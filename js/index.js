417eb72ff0f47c229e97064f9b9db7ebc33ac2e2
João Lima <joaopedro.donatolima@hotmail.com>
2024-11-13 12:07:05 -0300

    Adicionar interações por terminal e tratamento de exceções

--- js/index.js
+++ js/index.js
@@ -1,16 +1,32 @@
 const WebSocket = require('ws');
-const ws = new WebSocket("ws://localhost:3000");
+const readLineSync = require('readline-sync');
 
-ws.on('open', () => {
-    console.log("Conectado ao servidor");
+const selectedHost = readLineSync.question("Qual o IP de conexao? ");
+const selectedPort = readLineSync.question("Qual a porta de conexao? ");
+const tokenAccess = readLineSync.question("Qual o seu token de acesso? ");
 
-    ws.send(JSON.stringify({
-        type: "message",
-        data: "Hello World"
-    }));
-});
+try {
+    const ws = new WebSocket("ws://" + selectedHost + ":" + selectedPort  + "?token=" + tokenAccess);
 
-ws.onmessage = (event) => {
-    const obj = event.data;
-    console.log("Resposta do servidor: ", obj);
+    ws.on('error', (err) => {
+        console.error('Erro de conexão:', err);
+    });
+
+    ws.on('open', () => {
+        console.log("Conectado ao servidor!");
+
+        ws.send(JSON.stringify({
+            type: "message",
+            data: "Hello World"
+        }));
+
+        console.log("Uma mensagem foi enviada para o servidor!");
+    });
+
+    ws.onmessage = (event) => {
+        const obj = event.data;
+        console.log("Resposta do servidor: ", obj);
+    }
+} catch (e) {
+    console.error(`Um erro ocorreu: ${e.message}`);
 }
